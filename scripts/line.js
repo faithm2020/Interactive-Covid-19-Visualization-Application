5a95c4ef3255919195aba4c53904e6fd52900ca7
Artho Das <and2002@hw.ac.uk>
2024-04-01 03:49:29 +0400

    Added map and Line Graph

---
+++ scripts/line.js
@@ -0,0 +1,152 @@
+// Dimensions of graph
+const lineMargin = { top: 35, right: 30, bottom: 80, left: 80 };
+const lineWidth = 620 - lineMargin.left - lineMargin.right;
+const lineHeight = 400 - lineMargin.top - lineMargin.bottom;
+
+function lineGraph(data) {
+    d3.select(".line-svg").remove();
+
+    const lineMargin = { top: 35, right: 80, bottom: 80, left: 80 };
+    const lineWidth = 620 - lineMargin.left - lineMargin.right;
+    const lineHeight = 400 - lineMargin.top - lineMargin.bottom;
+
+    const timeStamps = data.map(d => d3.timeParse("%Y-%m-%d")(d.date));
+    const domain = d3.extent(timeStamps);
+
+    // filter the latest data out as it has bugs
+    data = data.filter(d => d3.timeParse("%Y-%m-%d")(d.date) < d3.timeParse("%Y-%m-%d")("2024-03-03"));
+
+    const x = d3.scaleTime().domain(domain).range([0, lineWidth]);
+
+    // Y-scale for total cases
+    const yTotalCases = d3.scaleLinear()
+        .domain([0, d3.max(data, d => +d.total_cases * 1.1)]) // Add 10% padding
+        .range([lineHeight, 0]);
+
+    // Y-scale for total deaths
+    const yTotalDeaths = d3.scaleLinear()
+        .domain([0, d3.max(data, d => +d.total_deaths * 1.1)]) // Add 10% padding
+        .range([lineHeight, 0]);
+
+    // append the svg object to the body of the page using line-viz ID.
+    const lineSvg = d3.select("#line-viz")
+        .append("svg")
+        .attr("width", lineWidth + lineMargin.left + lineMargin.right)
+        .attr("height", lineHeight + lineMargin.top + lineMargin.bottom)
+        .attr("class", "line-svg")
+        .append("g")
+        .attr("transform", `translate(${lineMargin.left},${lineMargin.top})`);
+
+    // X-axis
+    lineSvg.append("g")
+        .attr("transform", `translate(0, ${lineHeight})`)
+        .call(d3.axisBottom(x))
+        .call(g => g.append("text")
+            .attr("x", lineWidth)
+            .attr("y", lineMargin.bottom - 50)
+            .attr("fill", "#98C1D9")
+            .attr("text-anchor", "end")
+            .text("Timeline →"));
+
+    // Left Y-axis for total cases
+    lineSvg.append("g")
+        .call(d3.axisLeft(yTotalCases))
+        .call(g => g.append("text")
+            .attr("x", -lineMargin.left)
+            .attr("y", -15)
+            .attr("fill", "#98C1D9")
+            .attr("text-anchor", "start")
+            .text("↑ Total Covid-19 Cases"));
+
+    // Right Y-axis for total deaths
+    lineSvg.append("g")
+        .attr("transform", `translate(${lineWidth}, 0)`)
+        .call(d3.axisRight(yTotalDeaths))
+        .call(g => g.append("text")
+            .attr("x", 0)
+            .attr("y", -15)
+            .attr("fill", "red")
+            .attr("text-anchor", "end")
+            .text("↑ Total Covid-19 Deaths"));
+
+    // Line for total cases
+    lineSvg.selectAll(".line-total-cases")
+        .data(d3.group(data, d => d.location))
+        .enter()
+        .append("path")
+        .attr("class", "line-total-cases")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("d", d => d3.line()
+            .x(d => x(d3.timeParse("%Y-%m-%d")(d.date)))
+            .y(yTotalCases(0)) // Start from 0
+            .curve(d3.curveMonotoneX)(d[1])
+        )
+        .transition()
+        .duration(1500) // Animation duration
+        .attr("d", d => d3.line()
+            .x(d => x(d3.timeParse("%Y-%m-%d")(d.date)))
+            .y(d => yTotalCases(+d.total_cases))
+            .curve(d3.curveMonotoneX)(d[1])
+        );
+
+    // Line for total deaths
+    lineSvg.selectAll(".line-total-deaths")
+        .data(d3.group(data, d => d.location))
+        .enter()
+        .append("path")
+        .attr("class", "line-total-deaths")
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("d", d => d3.line()
+            .x(d => x(d3.timeParse("%Y-%m-%d")(d.date)))
+            .y(yTotalDeaths(0)) // Start from 0
+            .curve(d3.curveMonotoneX)(d[1])
+        )
+        .transition()
+        .duration(1500) // Animation duration
+        .attr("d", d => d3.line()
+            .x(d => x(d3.timeParse("%Y-%m-%d")(d.date)))
+            .y(d => yTotalDeaths(+d.total_deaths))
+            .curve(d3.curveMonotoneX)(d[1])
+        );
+
+    // Title
+    lineSvg.append('text')
+        .attr("x", lineWidth / 2)
+        .attr("y", lineHeight + 45)
+        .attr("text-anchor", "middle")
+        .attr("stroke", 1)
+        .style("font-size", "0.95em")
+        .style("fill", "black")
+        .text(() => {
+            return "Growth of Covid-19 overtime: " + data[0].location;
+        });
+
+    //Country search 
+    // const searchContainer = d3.select("#line-viz")
+    // .append("div")
+    // .attr("class", "search-container");
+
+    // searchContainer.append("input")
+    //     .attr("type", "text")
+    //     .attr("placeholder", "Enter country name...")
+    //     .attr("id", "country-input");
+
+    // searchContainer.append("button")
+    //     .text("Search")
+    //     .on("click", function() {
+    //         const countryName = d3.select("#country-input").property("value").toLowerCase();
+    //         const filteredData = mainData.filter(d => d.location.toLowerCase() === countryName);
+    //         lineGraph(filteredData);
+    //     });
+}
+
+document.getElementById("search-btn").addEventListener("click", function() {
+    // Get the input value
+    let countryName = document.getElementById("country-input").value;
+    const filteredData = mainData.filter(d => d.location.toLowerCase() === countryName.toLowerCase());
+    // Call the lineGraph function with the entered country name
+    lineGraph(filteredData);
+});
+  
---
+++ scripts/map.js
@@ -0,0 +1,196 @@
+const mapWidth = 52;
+const mapHeight = 80;
+
+  // The svg
+const mapSvg = d3.select("#map-viz")
+            .append("svg")
+            .attr("class", "map-svg")
+            .attr("width", `${mapWidth}vw`)
+            .attr("height",`${mapHeight}vh`);
+
+//Different map projections- 
+// geoEquirectangular()
+// geoMercerator()
+// geoEqualEarth()
+// Map and projection
+
+let mainData;
+let continentData;
+
+d3.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv").then(data => {
+
+    mainData = data;
+    continentData = data.filter(d => d.date === "2023-03-07" && ['OWID_AFR','OWID_ASI','OWID_EUR','OWID_NAM','OWID_OCE','OWID_SAM'].includes(d.iso_code));
+
+});
+
+const mapPath = d3.geoPath();
+const projection = d3.geoMercator()
+  .scale(125)
+  .center([0,20])
+  .translate([ window.innerWidth * mapWidth / 200 , window.innerHeight * mapHeight / 200]);
+
+  // Data and color scale
+const data = new Map();
+const colorScale = d3.scaleThreshold()
+  .domain([0, 1000, 10000, 50000, 100000, 200000, 300000, 500000])
+  .range(d3.schemeOranges[9]);
+// .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 20000000, 50000000])
+
+const selectedData = 'cases';
+
+let arr = [];
+// Load external data and boot
+function updateMap(selectedData){
+    let domainValues;
+    let rangeValues;
+    switch(selectedData) {
+    case 'cases':
+        domainValues = [0, 1000, 10000, 50000, 100000, 200000, 300000, 500000];
+        rangeValues = d3.schemeOranges[9]
+        break;
+    case 'deaths':
+        domainValues = [0,100,200,500,1000,2000,3000,5000];
+        rangeValues = d3.schemeReds[9]
+        break;
+    case 'vaccines':
+        domainValues = [0, 10, 20, 50,100,200,300,500];
+        rangeValues = d3.schemeBlues[9]
+        // Adjust your data processing logic for vaccines
+        break;
+    default:
+        domainValues = [0, 1000, 10000, 50000, 100000, 200000, 300000, 500000];
+        rangeValues = d3.schemeOranges[9]
+    }
+
+    colorScale.domain(domainValues);
+    colorScale.range(rangeValues);
+    Promise.all([
+    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
+    d3.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv",function (d) {
+        if (d.date === "2024-03-03") {
+            switch(selectedData) {
+            case 'cases':
+                data.set(d.iso_code, d.total_cases_per_million);
+                arr.push(d.total_cases_per_million);
+                break;
+            case 'deaths':
+                data.set(d.iso_code, d.total_deaths_per_million);
+                arr.push(d.total_deaths_per_million);
+                break;
+            case 'vaccines':
+                data.set(d.iso_code, d.total_vaccinations_per_hundred);
+                arr.push(d.total_vaccinations_per_hundred);
+                break;
+            default:
+                data.set(d.iso_code, d.total_cases_per_million); // Default to cases
+                arr.push(d.total_cases_per_million); // Push data to arr based on the default selection
+            }
+        };
+    })])
+    .then(function(loadData) {
+        let topo = loadData[0];
+
+        // filter antartica out of map
+        //topo.features = topo.features.filter(d => d.id !== 'ANT')
+
+        let mouseOver = function(event, d) {
+            mapTooltip.style("opacity", 1);
+            d3.selectAll(".Country")
+            .transition()
+            .duration(200)
+            .style("opacity", .5);
+            d3.selectAll(`.${event.target.classList[1]}`)        
+            .transition()
+            .duration(200)
+            .style("opacity", 1);
+        }
+
+        const mouseMove = (event, d) => {
+        mapTooltip
+            .html('<u>' + d.properties.name + '</u>' + "<br>" + parseInt(d.total) + " "+selectedData+" per million")
+            .style("position", "fixed")
+            .style("left", (event.x + 15) + "px")
+            .style("top", (event.y - (scrollY/5)) + "px");
+    };
+
+        let mouseLeave = function(event, d) {
+            mapTooltip.style("opacity", 0);
+            d3.selectAll(".Country")
+            .transition()
+            .duration(200)
+            .style("opacity", 1);
+        }
+        
+        let mouseClick = function(event, d) {
+            console.log("Mouse clicked on a country:", d); // Add this log message
+            if(d.id !== 'ATA' )
+                lineGraph(mainData.filter(datum => datum.iso_code === d.id))
+            else
+                lineGraph(mainData.filter(datum => datum.location === 'World'))
+                d3.select(`.${event.target.classList[1]}-gdp`)
+                .transition()
+                .delay(1000)
+                .duration(800)
+                .attr("stroke", "white")
+                .attr("stroke-width", 3)
+                .transition()
+                .delay(3000)
+                .duration(600)
+                .attr("stroke", "black")
+                .attr("stroke-width", );
+        }
+        
+        // create a tooltip
+        const mapTooltip = d3.select("#map-viz")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "0.3em");
+
+
+        // Draw the map
+        mapSvg.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .enter()
+        .append("path")
+        // draw each country
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        // set the color of each country
+        .attr("fill", function (d) {
+            d.total = data.get(d.id) || 0;
+            return colorScale(d.total);
+        })
+        .style("stroke", "black")
+        .attr("class", function(d){ return `Country ${d.id}` } )
+        .style("opacity", .8)
+        .on("mouseover", mouseOver )
+        .on("mousemove", mouseMove)
+        .on("click", mouseClick)
+        .on("mouseleave", mouseLeave );
+
+    });
+}
+
+updateMap(selectedData);
+
+document.getElementById("data-select").addEventListener("change", function() {
+    let selectedData = this.value;
+    // Call the loadData function with the newly selected data
+    updateMap(selectedData);
+  });
+//   document.getElementById("search-btn").addEventListener("click", function() {
+//     // Get the input value
+//     let countryName = document.getElementById("country-input").value;
+//     const filteredData = mainData.filter(d => d.location.toLowerCase() === countryName);
+//     // Call the lineGraph function with the entered country name
+//     lineGraph(filteredData);
+// });
+  
