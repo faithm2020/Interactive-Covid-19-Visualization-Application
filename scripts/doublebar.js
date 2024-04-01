e2133794ec1cd8ab604047a48b4557fb03cc8632
Faith Muraino <fm2020@hw.ac.uk>
2024-04-01 10:43:28 +0400

    double bar -temporary

--- scripts/doublebar.js
+++ scripts/doublebar.js
@@ -1,17 +1,16 @@
-//d3.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv").then(data => {
-//to load the dataset from the provided document
-    const data = [
-    {location: 'Afghanistan', population_density: 54.422, total_tests_per_thousand: 26.319},
-    {location: 'Albania', population_density: 104.871, total_tests_per_thousand: 21.852},
-    {location: 'Algeria', population_density: 17.348, total_tests_per_thousand: 17.025},
-    {location: 'United Arab Emirates', population_density: 112.442, total_tests_per_thousand: 20.861},
-    {location: 'United Kingdom', population_density: 272.898, total_tests_per_thousand: 37.746},
-    ];
+//to set the dimensions and margins of the graph
+const doubleBarMargin = {top: 25, right: 70, bottom: 40, left: 50};
+const doubleBarWidth = 600 - doubleBarMargin.left - doubleBarMargin.right;
+const doubleBarHeight = 400 - doubleBarMargin.top - doubleBarMargin.bottom;
 
-    //to set the dimensions and margins of the graph
-    const doubleBarMargin = {top: 25, right: 70, bottom: 40, left: 50};
-    const doubleBarWidth = 600 - doubleBarMargin.left - doubleBarMargin.right;
-    const doubleBarHeight = 400 - doubleBarMargin.top - doubleBarMargin.bottom;
+
+//to load the dataset from the provided document
+d3.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv").then(data => {
+
+    const uniqueLocations = Array.from(new Set(data.map(d => d.location))).slice(0, 7);
+    uniqueLocations.splice(1, 1); // Remove the second element
+    uniqueLocations.splice(3, 1); // Remove the fifth element
+    const limitedData = data.filter(d => uniqueLocations.includes(d.location));
     
     // Append the SVG object to the HTML element with id 'line-vaccine-viz'
     const doubleBarSVG = d3.select("#double-bar")
@@ -23,23 +22,23 @@
     
     //add x-axis for location(countries)
     const xAxis = d3.scaleBand()
-        .domain(data.map(d => d.location))
+        .domain(limitedData.map(d => d.location))
         .range([0, doubleBarWidth])
         .padding(0.1);
 
     //add the y-axis on the left side for population density
     const yAxisLeft = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.population_density)])
+        .domain([0, d3.max(limitedData, d => d.population_density)])
         .range([doubleBarHeight, 0]);
     
     //add the y-axis on the right side for total cases
     const yAxisRight = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.total_tests_per_thousand)])
+        .domain([0, d3.max(limitedData, d => d.people_fully_vaccinated)])
         .range([doubleBarHeight, 0]);
     
     // Create bars for population density
     doubleBarSVG.selectAll(".bar1")
-        .data(data)
+        .data(limitedData)
         .enter().append("rect")
         .attr("class", "bar1")
         .attr("x", d => xAxis(d.location))
@@ -50,13 +49,13 @@
 
     //Create bars for total cases
     doubleBarSVG.selectAll(".bar2")
-        .data(data)
+        .data(limitedData)
         .enter().append("rect")
         .attr("class", "bar2")
         .attr("x", d => xAxis(d.location) + xAxis.bandwidth() / 2)
         .attr("width", xAxis.bandwidth() / 2)
-        .attr("y", d => yAxisRight(d.total_tests_per_thousand))
-        .attr("height", d => doubleBarHeight - yAxisRight(d.total_tests_per_thousand))
+        .attr("y", d => yAxisRight(d.people_fully_vaccinated))
+        .attr("height", d => doubleBarHeight - yAxisRight(d.people_fully_vaccinated))
         .attr("fill", "orange");
     
     // Add the X Axis
@@ -86,7 +85,7 @@
         .attr("x",0 - (doubleBarHeight / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
-        .text("Population Density per sq.km");
+        .text("Population Density (%)");
 
     // Add right Y axis label
     doubleBarSVG.append("text")
@@ -95,7 +94,7 @@
         .attr("x",0 - (doubleBarHeight / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
-        .text("Total Tests Per Thousand");
+        .text("People Fully Vaccinated");
      
-//})
+})
 
